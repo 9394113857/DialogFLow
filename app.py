0b3015d9bf7257f396e09043fc2517bd65212a07
9394113857 <81347796+9394113857@users.noreply.github.com>
2022-03-30 16:39:12 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,35 @@
+# Flask app should start in global layout
+import os
+
+import flask as flask
+from flask import send_from_directory, request
+
+app = flask.Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Hello World"
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(force=True)
+    print(req)
+
+    return {
+        'fulfillmentText': 'Hello from the bot world'
+    }
+
+
+if __name__ == "__main__":
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
